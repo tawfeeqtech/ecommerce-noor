c0c66ce1ecc9d767d973e9715f7b68811e98a7c7
tawfeeq <tawfeeq.tech@gmail.com>
2023-06-24 11:16:49 +0300

    Display analytics in admin dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard');
+        $totalProducts = Product::count();
+        $totalCategories = Category::count();
+        $totalBrand = Brand::count();
+
+        $totalAllUsers = User::count();
+        $totalUser = User::where('role_as','0')->count();
+        $totalAdmin = User::where('role_as','1')->count();
+
+        $todayDate = Carbon::now()->format('d-m-Y');
+        $thisMonth = Carbon::now()->format('m');
+        $thisYear  = Carbon::now()->format('Y');
+
+        $totalOrder = Order::count();
+        $todayOrder = Order::whereDate('created_at',$todayDate)->count();
+        $thisMonthOrder = Order::whereMonth('created_at',$thisMonth)->count();
+        $thisYearOrder = Order::whereYear('created_at',$thisYear)->count();
+
+
+        return view('admin.dashboard',[
+            'totalProducts' => $totalProducts,
+            'totalCategories' => $totalCategories,
+            'totalBrand' => $totalBrand,
+            'totalAllUsers' => $totalAllUsers,
+            'totalUser' => $totalUser,
+            'totalAdmin' => $totalAdmin,
+            'totalOrder' => $totalOrder,
+            'todayOrder' => $todayOrder,
+            'thisMonthOrder' => $thisMonthOrder,
+            'thisYearOrder' => $thisYearOrder,
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,13 +7,100 @@
             <h2 class="alert alert-success">{{session('message')}}</h2>
         @endif
 
-      <div class="d-flex justify-content-between flex-wrap">
-        <div class="d-flex align-items-end flex-wrap">
-          <div class="me-md-3 me-xl-5">
+        <div class="me-md-3 me-xl-5">
             <h2>Dashboard</h2>
-          </div>
+            <hr>
         </div>
-      </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white">
+                    <label>Total Orders</label>
+                    <h1>{{$totalOrder}}</h1>
+                    <a href="{{route('admin.orders.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white">
+                    <label>Today Orders</label>
+                    <h1>{{$todayOrder}}</h1>
+                    <a href="{{route('admin.orders.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white">
+                    <label>This Month Orders</label>
+                    <h1>{{$thisMonthOrder}}</h1>
+                    <a href="{{route('admin.orders.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-danger text-white">
+                    <label>Year Orders</label>
+                    <h1>{{$thisYearOrder}}</h1>
+                    <a href="{{route('admin.orders.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white ">
+                    <label>Total Products</label>
+                    <h1>{{$totalProducts}}</h1>
+                    <a href="{{route('products.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white">
+                    <label>Total Categories</label>
+                    <h1>{{$totalCategories}}</h1>
+                    <a href="{{route('category.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white">
+                    <label>Total Brand</label>
+                    <h1>{{$totalBrand}}</h1>
+                    <a href="{{route('brand.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body bg-primary text-white">
+                    <label>Total All Users</label>
+                    <h1>{{$totalAllUsers}}</h1>
+                    <a href="{{route('users.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-success text-white">
+                    <label>Total Users</label>
+                    <h1>{{$totalUser}}</h1>
+                    <a href="{{route('users.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card card-body bg-warning text-white">
+                    <label>Total Admin Users</label>
+                    <h1>{{$totalAdmin}}</h1>
+                    <a href="{{route('users.index')}}" class="text-white">View</a>
+                </div>
+            </div>
+        </div>
+
     </div>
-  </div>
+</div>
 @endsection
